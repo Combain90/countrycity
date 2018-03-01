0fabc1254d05bfe67f6f743c0cb3e8968ce0af88
ivan.maltese@objectmethod.it <ivan.maltese@objectmethod.it>
2018-03-01 15:56:26 +0100

    corre

--- src/it/objectmethod/countrycity/principale/controller/CountryController.java
+++ src/it/objectmethod/countrycity/principale/controller/CountryController.java
@@ -3,8 +3,12 @@ package it.objectmethod.countrycity.principale.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import it.objectmethod.countrycity.principale.model.dao.DaoCountry;
 import it.objectmethod.countrycity.principale.model.pojo.CountryBean;
@@ -23,10 +27,41 @@ public class CountryController{
 		return lista; // vado alla vista desiderata
 	}
 	
-	@RequestMapping("/{continent}")
+	@RequestMapping("/{continent}/list")
 	public List<CountryBean> countriesByContinent(@PathVariable("continent")String continente) {
 		List<CountryBean> lista=null;
 		lista=daoCountry.getCountriesByContinent(continente);
 		return lista;
 	}
+	
+//	@RequestMapping(value="/{countryCode}", method=RequestMethod.GET)
+//	public CountryBean getCountry() {
+//		
+//	}
+//	
+//	@RequestMapping(value="/{countryCode}", method=RequestMethod.DELETE)
+//	public int deleteCountry() {
+//		
+//	}
+//	
+//	@RequestMapping(value="/{countryCode}", method=RequestMethod.PUT)
+//	public CountryBean addEditCountry() {
+//		
+//	}
+	
+//	@GetMapping(value="/{countryCode}")
+//	public CountryBean getCountry() {
+//		
+//	}
+//	
+//	@DeleteMapping(value="/{countryCode}")
+//	public int deleteCountry() {
+//		
+//	}
+//	
+//	@PutMapping(value="/{countryCode}")
+//	public CountryBean addEditCountry() {
+//		
+//	}
+
 }
